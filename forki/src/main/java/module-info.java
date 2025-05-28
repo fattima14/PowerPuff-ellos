9843cab9eb2179e3ebff38c891e92d36b1f492ec
fattima14 <fatima.jurado@alumnos.uacm.edu.mx>
2025-05-27 20:43:02 -0600

    Elimine por error

--- forki/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module mx.edu.uacm.is.slt.ds.forki {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens mx.edu.uacm.is.slt.ds.forki to javafx.fxml;
-    opens mx.edu.uacm.is.slt.ds.forki.clases to javafx.base; 
-
-    exports mx.edu.uacm.is.slt.ds.forki;
-}
